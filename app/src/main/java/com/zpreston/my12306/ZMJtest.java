91e98a48dd9d585aa2eb3828d5fa753722b8b385
Zprestonx <Zprestonx@gmail.com>
2016-07-20 09:43:11 +0800

    测试 钟淼君

---
+++ app/src/main/java/com/zpreston/my12306/ZMJtest.java
@@ -0,0 +1,7 @@
+package com.zpreston.my12306;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class ZMJtest {
+}
--- app/src/main/java/com/zpreston/my12306/activity/SplashActivity.java
+++ app/src/main/java/com/zpreston/my12306/activity/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.zpreston.my12306.activity;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,13 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //在加载布局文件前判断是否是首次打开
+        setContentView(R.layout.activity_splash);//文件自带，是否要删？
+        SharedPreferences appInfo=getSharedPreferences("appIinfo",MODE_WORLD_READABLE);
+        boolean isFirst=appInfo.getBoolean("isFirst",true);
+        if(isFirst){
+            SharedPreferences.Editor editor=appInfo.edit();
+
+        }
     }
 }
