e9d2660f15a5f13024f876ac46377a62d0780b8a
蔡文达 <351770169@qq.com>
2016-07-22 12:43:32 +0800

    蔡文达:修改布局文件内容

---
+++ app/src/main/java/com/zpreston/my12306/activity/ticket/Ticket3_AddActivity.java
@@ -0,0 +1,58 @@
+package com.zpreston.my12306.activity.ticket;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.zpreston.my12306.R;
+import com.zpreston.my12306.activity.mine.ContactAddActivity;
+
+public class Ticket3_AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket3__add);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* 使用inflate方法把布局文件中定义的菜单加载给第二个参数对应的menu对象 */
+        getMenuInflater().inflate(R.menu.mine_contact_menu1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /* 菜单栏选项点击事件 */
+        switch(item.getItemId()){
+            case R.id.edit_item:
+                Toast.makeText(this,"you clicked Edit", Toast.LENGTH_SHORT).show();
+                /* 编辑联系人——原先的编辑框均变为可编辑 */
+
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"you clicked Remove",Toast.LENGTH_SHORT).show();
+                /* 删除联系人——从数据库删除并更新联系人列表 */
+                Intent intent=new Intent().setClass(Ticket3_AddActivity.this,Ticket3Activity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.add_item:
+                Toast.makeText(this,"you clicked Add", Toast.LENGTH_SHORT).show();
+
+            default:
+                Toast.makeText(Ticket3_AddActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
