c076e13538e55b76610d7551313fb27140564bd8
xiaohuasheng <775079852@qq.com>
2016-07-24 16:47:09 +0800

    在Util.java里增加了getEmail和getPassword方法，使用时先创建Util类对象，构造方法需要传上下文 对象名.getEmail() 调用，结果为email

--- app/src/main/java/com/zpreston/my12306/util/Util.java
+++ app/src/main/java/com/zpreston/my12306/util/Util.java
@@ -1,11 +1,17 @@
 package com.zpreston.my12306.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by user on 2016/7/21.
  */
 public class Util {
+    private Context context;
     //添加一个加密的方法，用来加密密码
     public static String encryption(String plaintext)
     {
@@ -18,6 +24,33 @@ public class Util {
         Log.e(tag, "*********************************"+tag+"Start*********");
         Log.e(tag, "*********************************"+str+"**************");
         Log.e(tag, "*********************************"+tag+"End**************");
+    }
+
+    public Util(Context context)
+    {
+       this.context = context;
+    }
+
+    /*
+    调用getEmail()方法前先创建Util对象，构造函数要传入当前的上下文
+    * */
+    public String getEmail()
+    {
+        SharedPreferences sp = context.getSharedPreferences("user", Context.MODE_PRIVATE);
+        //第二个参数是缺省值，找不到的时候用这个
+        String email = sp.getString("name", "");
+        return email;
+    }
+
+    /*
+    调用getPassword()方法前先创建Util对象，构造函数要传入当前的上下文
+    * */
+    public String getPassword()
+    {
+        SharedPreferences sp = context.getSharedPreferences("user", Context.MODE_PRIVATE);
+        //第二个参数是缺省值，找不到的时候用这个
+        String password = sp.getString("pwd", "");
+        return password;
     }
 
 
